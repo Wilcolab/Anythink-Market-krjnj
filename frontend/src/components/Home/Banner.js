52d08f50ab60376651cc1c7baab98bc9aa216a36
Hussain Shaikh <zakirroll@gmail.com>
2022-09-28 23:32:40 +0530

    Added hidden searchbar

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 import Search from "./Search";
 const Banner = () => {
+  const [hidden, setHidden] = useState(true);
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" height="10" />
         <div className="d-flex justify-content-center align-items-center">
-          <span id="get-part">A place to get</span>
-          <Search />
+          <span>A place to </span>
+          <span
+            id="get-part"
+            onClick={() => {
+              setHidden(false);
+            }}
+          >
+            {"\u00a0"}
+            get
+            {"\u00a0"}
+          </span>
+          <span hidden={hidden}>
+            <Search />
+          </span>
           <span> the cool stuff.</span>
         </div>
       </div>
