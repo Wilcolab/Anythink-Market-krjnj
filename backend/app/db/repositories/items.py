e9f66b58d91151b626a5b4f7c89eaa7a86a2383e
Hussain Shaikh <46746707+hussainshaikh12@users.noreply.github.com>
2023-03-21 23:20:22 +0530

    added dall-e support (#22)

--- backend/app/db/repositories/items.py
+++ backend/app/db/repositories/items.py
@@ -24,6 +24,14 @@ SLUG_ALIAS = "slug"
 
 CAMEL_OR_SNAKE_CASE_TO_WORDS = r"^[a-z\d_\-]+|[A-Z\d_\-][^A-Z\d_\-]*"
 
+import os
+
+import openai
+
+from dotenv import load_dotenv
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 class ItemsRepository(BaseRepository):  # noqa: WPS214
     def __init__(self, conn: Connection) -> None:
@@ -43,6 +51,13 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
         tags: Optional[Sequence[str]] = None,
     ) -> Item:
         async with self.connection.transaction():
+            if not image:
+                response = openai.Image.create(
+                    prompt=title,
+                    n=1,
+                    size="256x256",
+                )
+                image = response["data"][0]["url"]
             item_row = await queries.create_new_item(
                 self.connection,
                 slug=slug,
